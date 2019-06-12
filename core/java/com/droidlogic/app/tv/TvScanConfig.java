652fd0dc13cda97775d85b3a69046813e16e2a95
hongchao.yin <hongchao.yin@amlogic.com>
2019-06-12 11:02:51 +0800

    droidlogic-tv: add license declaration [2/3]
    PD#SWPL-9663
    
    Problem:
    clean audio no license file
    
    Solution:
    add license declaration
    
    Verify:
    verrify by marconi
    
    Change-Id: Ie2eaccfd634ce72c97023702aae629a581cb66ff
    Signed-off-by: hongchao.yin <hongchao.yin@amlogic.com>
    Signed-off-by: Luan Yuan <luan.yuan@amlogic.com>

--- core/java/com/droidlogic/app/tv/TvScanConfig.java
+++ core/java/com/droidlogic/app/tv/TvScanConfig.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2018 Amlogic, Inc. All rights reserved.
+ *
+ * This source code is subject to the terms and conditions defined in the
+ * file 'LICENSE' which is part of this source code package.
+ *
+ * Description: JAVA file
+ */
 
 package com.droidlogic.app.tv;
 
